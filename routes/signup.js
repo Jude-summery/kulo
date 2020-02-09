8ca6285f529e754f87c62549b7246778c0bb8527
Jude-summery <329543240@qq.com>
2020-02-09 22:10:42 +0800

    feat: 注册接口

--- routes/signup.js
+++ routes/signup.js
@@ -7,68 +7,24 @@ const router = express.Router()
 const UserModel = require('../models/users')
 const checkNotLogin = require('../middlewares/check').checkNotLogin
 
-// POST /signup 用户注册
+// POST /api/signup 用户注册
 router.post('/', function (req, res, next) {
-  // const name = req.fields.name
-  // const gender = req.fields.gender
-  // const bio = req.fields.bio
-  // const avatar = req.files.avatar.path.split(path.sep).pop()
-  // let password = req.fields.password
-  // const repassword = req.fields.repassword
+  let { name, password, confirm } = req.fields
   const result = {
-    // `data` 由服务器提供的响应, 需要进行解析才能获取
-    data: {},
-  
-    // `status` 来自服务器响应的 HTTP 状态码
+    data: null,
     status: 200,
-  
-    // `statusText` 来自服务器响应的 HTTP 状态信息
-    statusText: 'OK',
-  
-    // `headers` 服务器响应的头
-    headers: {},
+    statusText: 'success',
+    message: ''
   }
-  
+  // 明文密码加密
+  password = sha1(password)
+  // 待写入数据库的用户信息
+  let user = {
+    name: name,
+    password: password
+  }
   res.send(result)
-  // // 校验参数
-  // try {
-  //   if (!(name.length >= 1 && name.length <= 10)) {
-  //     throw new Error('名字请限制在 1-10 个字符')
-  //   }
-  //   if (['m', 'f', 'x'].indexOf(gender) === -1) {
-  //     throw new Error('性别只能是 m、f 或 x')
-  //   }
-  //   if (!(bio.length >= 1 && bio.length <= 30)) {
-  //     throw new Error('个人简介请限制在 1-30 个字符')
-  //   }
-  //   if (!req.files.avatar.name) {
-  //     throw new Error('缺少头像')
-  //   }
-  //   if (password.length < 6) {
-  //     throw new Error('密码至少 6 个字符')
-  //   }
-  //   if (password !== repassword) {
-  //     throw new Error('两次输入密码不一致')
-  //   }
-  // } catch (e) {
-  //   // 注册失败，异步删除上传的头像
-  //   fs.unlink(req.files.avatar.path)
-  //   req.flash('error', e.message)
-  //   return res.redirect('/signup')
-  // }
-
-  // // 明文密码加密
-  // password = sha1(password)
-
-  // // 待写入数据库的用户信息
-  // let user = {
-  //   name: name,
-  //   password: password,
-  //   gender: gender,
-  //   bio: bio,
-  //   avatar: avatar
-  // }
-  // // 用户信息写入数据库
+  //用户信息写入数据库
   // UserModel.create(user)
   //   .then(function (result) {
   //     // 此 user 是插入 mongodb 后的值，包含 _id
@@ -76,20 +32,15 @@ router.post('/', function (req, res, next) {
   //     // 删除密码这种敏感信息，将用户信息存入 session
   //     delete user.password
   //     req.session.user = user
-  //     // 写入 flash
-  //     req.flash('success', '注册成功')
-  //     // 跳转到首页
-  //     res.redirect('/posts')
   //   })
   //   .catch(function (e) {
-  //     // 注册失败，异步删除上传的头像
-  //     fs.unlink(req.files.avatar.path)
-  //     // 用户名被占用则跳回注册页，而不是错误页
   //     if (e.message.match('duplicate key')) {
-  //       req.flash('error', '用户名已被占用')
-  //       return res.redirect('/signup')
+  //       result.statusText = 'error'
+  //       result.message = '用户名已被占用'
   //     }
-  //     next(e)
+  //   })
+  //   .then(() => {
+  //     res.send(result)
   //   })
 })
 
